55ee6edfa178e30b9233982ed872eab67dc8b7bf
DANIELSM864 <danisoleram@gmail.com>
2023-10-21 13:35:06 -0600

    Projecto Aplicaciones Web

---
+++ OneDrive/Documents/Desarrollo Web/Anasovi/src/main/java/com/Anasovi/Anasovi/AnasoviApplication.java
@@ -0,0 +1,13 @@
+package com.Anasovi.Anasovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnasoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnasoviApplication.class, args);
+	}
+
+}
---
+++ OneDrive/Documents/Desarrollo Web/Anasovi/src/test/java/com/Anasovi/Anasovi/AnasoviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anasovi.Anasovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnasoviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
