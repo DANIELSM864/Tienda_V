bbb81e401a38694cdfc1f70513fdb1460148b241
DANIELSM864 <danisoleram@gmail.com>
2023-11-07 15:36:02 -0600

    productos crud

---
+++ OneDrive/Documents/NetBeansProjects/TiendaD/src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,25 @@
+
+package com.tienda.service;
+
+/**
+ *
+ * @author danis
+ */
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-e1fc7.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-e1fc7-firebase-adminsdk-5ixzl-9cd78f490a.json";
+}
