8cc3f43542ef238107b2fadbf6390ab726bc1790
DANIELSM864 <danisoleram@gmail.com>
2023-12-08 01:24:51 -0600

    Semana 13 reportes en la pagina

---
+++ OneDrive/Documents/NetBeansProjects/TiendaD/src/main/java/com/tienda/service/ReporteService.java
@@ -0,0 +1,20 @@
+
+package com.tienda.service;
+
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+
+public interface ReporteService {
+    //se define la firma del metodo que genera los reportes, con los sigueintes parametros
+    //1. reporte. Nombre del archivo de reportes (.jasper)
+    //2. parametros: un map que contiene los parametros del reporte si los ocupa
+    //3. tipo: es el tipo de reporte: vPdf, Pdf, Xls, Csv
+    public ResponseEntity<Resource> generaReporte(
+    String reporte,
+            Map<String, Object> parametros,
+            String tipo) throws IOException; 
+    
+}
